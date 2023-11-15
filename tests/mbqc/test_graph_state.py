a98a9b3b230b2993faf5517d4b48be3df020d797
Luis Mantilla <luismantilla99@outlook.com>
2023-11-14 19:10:33 -0500

    add couple of tests of gs module

--- tests/mbqc/test_graph_state.py
+++ tests/mbqc/test_graph_state.py
@@ -1,10 +1,23 @@
-# Copyright (C) [2023] Luis Mantilla
+# Copyright 2023 Luis Mantilla
 #
-# This program is released under the GNU GPL v3.0 or later.
-# See <https://www.gnu.org/licenses/> for details.
+# Licensed under the Apache License, Version 2.0.
+# See <http://www.apache.org/licenses/LICENSE-2.0> for details.
 """Tests for the graph state module."""
 
 import pytest
-from mentpy.mbqc import mbqcircuit
+from mentpy.mbqc.states.graphstate import GraphState
 
-# write tests
+
+def test_create_graphstate():
+    """Test the creation of a graph state."""
+    my_graph = GraphState()
+    my_graph.add_edges_from([(0, 1), (1, 2), (2, 3), (3, 4)])
+    assert my_graph.number_of_nodes() == 5
+
+
+def test_pauli_stabilizers():
+    """Test the generation of the stabilizers of a graph state."""
+    my_graph = GraphState()
+    my_graph.add_edges_from([(0, 1), (1, 2), (2, 3), (3, 4)])
+    stabs = my_graph.stabilizers()
+    assert len(stabs) == 5
