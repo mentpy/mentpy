3353fea897039dccdef13abb4922ef9a223ee552
Luis Mantilla <luismantilla99@outlook.com>
2024-02-02 11:39:26 -0500

    fix flow space module

--- mentpy/utils/flow_space.py
+++ mentpy/utils/flow_space.py
@@ -3,35 +3,47 @@
 # Licensed under the Apache License, Version 2.0.
 # See <http://www.apache.org/licenses/LICENSE-2.0> for details.
 """A module to study graphs with flow."""
-from mentpy.mbqc import MBQCircuit
-
-import networkx as nx
 import itertools
+import networkx as nx
+
+from mentpy.mbqc import MBQCircuit, GraphState
 
 
 class FlowSpace:
-    r"""The flow space graph of a MBQCGraph.
+    r"""The flow space graph of a MBQCircuit.
 
-    Each node corresponds to a possible graph over ``len(graph_state)`` qubits.
+    Each node corresponds to a possible graph over ``n_qubits`` qubits.
     Each edge between nodes represent going from one graph to another via adding or removing edges.
 
-    .. note::  ``flow_space()`` will only work for MBQCGraph with less
+    Args
+    ----
+    n_qubits: int
+        The number of qubits in the graph state.
+    input_nodes: list
+        The input nodes of the graph state.
+    output_nodes: list
+        The output nodes of the graph state.
+
+    .. note::  ``flow_space()`` will only work for MBQCircuit with less
     than 8 qubits.
     """
 
-    def __init__(self, graph_state: MBQCircuit, allow_any_size_graphs: bool = False):
+    def __init__(
+        self, n_qubits, input_nodes, output_nodes, allow_any_size_graphs: bool = False
+    ):
         """Creates the flow graph space of a graph state circuit."""
 
-        if len(graph_state) > 7 and (not allow_any_size_graphs):
+        if n_qubits > 7 and (not allow_any_size_graphs):
             raise UserWarning(
-                f"Expected a graph_state of size 7 or less, but {len(graph_state)} "
+                f"Expected a graph_state of size 7 or less, but {n_qubits} "
                 "was given."
             )
 
-        self.number_nodes = len(graph_state)
-        self.input_nodes = graph_state.input_nodes
-        self.output_nodes = graph_state.output_nodes
-        total_graph, wf_graph, wof_graph = self.all_graphs_graph()
+        self.number_nodes = n_qubits
+        self.input_nodes = input_nodes
+        self.output_nodes = output_nodes
+
+        total_graph, wf_graph, wof_graph = self._generate_graph_spaces()
         self.total_graph_space = total_graph
         self.flow_graph_space = wf_graph
         self.no_flow_graph_space = wof_graph
@@ -43,48 +55,82 @@ class FlowSpace:
             f"and {self.output_nodes} output nodes."
         )
 
-    def generator_all_graphs(self):
-        """Returns a generator that generates all possible graphs for :math:`n` ordered nodes."""
+    def _generate_all_graphs(self):
+        """Generator that yields all possible graphs for the specified number of nodes."""
         n = self.number_nodes
-        total_n = int(n * (n - 1) / 2)
-        completegraph = nx.complete_graph(n)
-        its = itertools.product([0, 1], repeat=total_n)
-        for j in its:
-            g = nx.Graph()
-            g.add_nodes_from(list(range(n)))
-            edgs = [edg for i, edg in enumerate(list(completegraph.edges())) if j[i]]
-            g.add_edges_from(edgs)
+        total_edges = int(n * (n - 1) / 2)
+        complete_graph = nx.complete_graph(n)
+        edge_combinations = itertools.product([0, 1], repeat=total_edges)
+
+        for edge_selection in edge_combinations:
+            g = GraphState()
+            g.add_nodes_from(range(n))
+            edges = [
+                edge
+                for i, edge in enumerate(complete_graph.edges())
+                if edge_selection[i]
+            ]
+            g.add_edges_from(edges)
             yield g
 
-    def all_graphs_graph(self):
-        """Returns a tuple with three graphs.
+    def _generate_graph_spaces(self):
+        """
+        Generates the total graph space and its subgraphs for states with and without flow.
 
-        The first graph is the graph corresponding to all possible graphs.
-        The second is the subgraph of all graphs with flow. The third is the
-        subgraph of all graphs without flow."""
-
-        graphs_list = list(self.generator_all_graphs())
+        Returns:
+            tuple: A tuple containing three nx.Graph objects (total_graph_space, flow_graph_space, no_flow_graph_space).
+        """
+        graphs_list = list(self._generate_all_graphs())
         graph_space = nx.Graph()
-        wflow = []
-        woflow = []
-        for ind, g in enumerate(graphs_list):
-            gs = MBQCircuit(
-                g, input_nodes=self.input_nodes, output_nodes=self.output_nodes
+        with_flow, without_flow = [], []
+
+        for idx, graph in enumerate(graphs_list):
+            mbqc_graph = MBQCircuit(
+                graph, input_nodes=self.input_nodes, output_nodes=self.output_nodes
             )
+            layers = mbqc_graph.gflow.layers
 
-            fl = gs.flow
-
-            if fl is not None:
-                graph_space.add_node(ind, flow=True, mbqc_circuit=gs)
-                wflow.append(ind)
+            if layers is not None:
+                graph_space.add_node(idx, flow=True, mbqc_circuit=mbqc_graph)
+                with_flow.append(idx)
             else:
-                graph_space.add_node(ind, flow=False, mbqc_circuit=gs)
-                woflow.append(ind)
+                graph_space.add_node(idx, flow=False, mbqc_circuit=mbqc_graph)
+                without_flow.append(idx)
 
-        for ind1, g1 in enumerate(graphs_list):
-            for ind2, g2 in enumerate(graphs_list):
-                if ind1 < ind2:
-                    if len(set(g2.edges()).symmetric_difference(set(g1.edges()))) == 1:
-                        graph_space.add_edge(ind1, ind2)
+        for (idx1, graph1), (idx2, graph2) in itertools.combinations(
+            enumerate(graphs_list), 2
+        ):
+            if len(set(graph2.edges()).symmetric_difference(graph1.edges())) == 1:
+                graph_space.add_edge(idx1, idx2)
 
-        return graph_space, graph_space.subgraph(wflow), graph_space.subgraph(woflow)
+        return (
+            graph_space,
+            graph_space.subgraph(with_flow),
+            graph_space.subgraph(without_flow),
+        )
+
+
+if __name__ == "__main__":
+    import cProfile
+    import pstats
+    import os
+    import subprocess
+
+    n_qubits = 4
+    input_nodes = [0]
+    output_nodes = [3]
+    profile_filename = "profile_stats.prof"
+    cProfile.run("FlowSpace(n_qubits, input_nodes, output_nodes)", profile_filename)
+
+    dot_filename = "profile_output.dot"
+    png_filename = "profile_output.png"
+    with open(dot_filename, "w") as f:
+        subprocess.run(["gprof2dot", "-f", "pstats", profile_filename], stdout=f)
+
+    # Convert .dot to .png
+    subprocess.run(["dot", "-Tpng", dot_filename, "-o", png_filename])
+
+    # Optional: Remove the .dot file if not needed
+    os.remove(dot_filename)
+
+    print(f"Profile image saved as {png_filename}")
