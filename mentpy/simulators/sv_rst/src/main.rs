e280c2afe5d913f27969d7421055965fe10901a7
Luis Mantilla <luismantilla99@outlook.com>
2024-04-30 15:43:28 -0400

    small change

--- mentpy/simulators/sv_rst/src/main.rs
+++ mentpy/simulators/sv_rst/src/main.rs
@@ -1,3 +1,36 @@
 fn main() {
     println!("Hello, world!");
+
+    let mut jacobian = vec![0.0; 14*1];
+    let res = drun(&angles, &jacobian, 1.0);
+    println!("{:?}", res);
+
+    // for i in 0..10 {
+    //     let mut dout = vec![0.0; 14];
+    //     dout[i] = 1.0;
+    //     drun(&angles, &mut dangles, &mut out, &mut dout);
+    //     for i in 0..14 {
+    //         jacobian[i*10 + i] = dangles[i];
+    //     }
+    // }
 }
+
+#[autodiff(drun, Reverse, Duplicated, Active)]
+fn sum_logistic(x: &[f64]) -> f64 {
+    let mut sum = 0.0;
+    for i in 0..x.len() {
+        sum += 1.0 / (1.0 + (-x[i]).exp());
+    }
+    return sum;
+}
+
+//#[autodiff(drun, Reverse, Duplicated, Active)]
+fn run(angles: &[f64]) -> f64 {
+    // let (out, out2) = run(angles);
+    // let res = userfn(out2);
+    // return res;
+    todo!();
+}
+
+
+
